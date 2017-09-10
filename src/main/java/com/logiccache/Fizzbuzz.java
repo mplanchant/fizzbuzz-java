fcf066a9673b6458d9dfcd69c702000887d8cfad
Matthew Planchant <matt@logiccache.co.uk>
2017-09-10 13:54:59 +0100

    Step one

---
+++ src/main/java/com/logiccache/Fizzbuzz.java
@@ -0,0 +1,38 @@
+package com.logiccache;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+
+public class Fizzbuzz {
+
+    private static final String FIZZ = "fizz";
+    private static final String BUZZ = "buzz";
+
+    public static List<String> of(int start, int end) {
+        checkArgument(start > 0, "Argument start was %s but expected to be greater than zero", start);
+        checkArgument(end >= start, "Argument end was %s but expected to be greater than or equal to start", end);
+        return IntStream.rangeClosed(start, end)
+                .mapToObj(Fizzbuzz::of)
+                .collect(Collectors.toList());
+    }
+
+    private static String of(int number) {
+        StringBuilder builder = new StringBuilder();
+        if (divisibleByThree(number)) builder.append(FIZZ);
+        if (divisibleByFive(number)) builder.append(BUZZ);
+        if (builder.length() == 0) builder.append(number);
+        return builder.toString();
+    }
+
+    private static boolean divisibleByThree(Integer number) {
+        return number % 3 == 0;
+    }
+
+    private static boolean divisibleByFive(Integer number) {
+        return number % 5 == 0;
+    }
+}
